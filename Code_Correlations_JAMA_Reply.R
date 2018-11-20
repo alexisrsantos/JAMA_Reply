a60cc94dbc8925236563385c6cc71f20f47fd820
Alexis R. Santos-Lozada <appdemography@gmail.com>
2018-11-19 22:15:58 -0500

    Included the subset of observation
    Only keep observations before 2017 for Correlation Analysis.

--- Code_Correlations_JAMA_Reply.R
+++ Code_Correlations_JAMA_Reply.R
@@ -4,7 +4,10 @@ options(digits=2,scipen=999)
 #Correlation Analysis for JAMA Reply to Cruz-Cano and Mead
 
 #Import Deaths
-deaths <- read.csv("PATH GOES HERE/Puerto_Rico_Deaths_00_17.csv")
+deaths <- read.csv("PATH GOES HERE/Puerto_Rico_Deaths_00_16.csv")
+
+#Just in case the file has 2017 observations
+deaths<-subset(deaths,Year<2017)
 
 ###This is the analysis using the data for the 2000-2016 period
 cor.test(x=deaths$Pop_Est,y=deaths$Total_Deaths,alternative = "two.sided",method ="pearson",conf.level = 0.95)
