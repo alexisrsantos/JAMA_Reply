23b67bc59b893c20a5cf252e80b1cda2f654ea08
Alexis R. Santos-Lozada <appdemography@gmail.com>
2018-11-26 12:52:33 -0500

    Add files via upload

--- OLS_Figure_Repository.R
+++ OLS_Figure_Repository.R
@@ -53,3 +53,51 @@ excess<-excess_data$Period_Deaths-excess_data$upr
 
 #Show excess calculation
 excess
+
+
+###########################################################################
+# Supplemental analysis: Using the mean imputation for October 2014 deaths#
+###########################################################################
+
+model2<-lm(Period_Deaths_Imputed~(Year)+Hurricane_Maria,data=deaths_raw)
+
+#Here we create our new dataset with years, and building a no-Hurricane Maria scenario
+new_year<-data.frame(Year=c(2000:2017),Hurricane_Maria=rep(0,18))
+
+#In this line we predict the values taking away the shock of Hurricane Maria
+pred.int2<-predict(model2,newdata=new_year,interval="confidence")
+
+#Here, we turn the predicted values in a dataset
+pred.int2<-as.data.frame(pred.int2)
+
+#Here, we create "mydata" which merges the original data and the predicted values
+mydata2<-cbind(deaths_raw,pred.int2)
+
+#Here we visualize the data, 2017 is a clear outlier from the 95% C.I. of the mean
+# Deaths for 2017 are also an outlier for the prediction interval generated from the time series
+p <- ggplot(mydata2, aes(Year, Period_Deaths)) +
+  geom_point() +
+  stat_smooth(method = lm) +geom_line(aes(y = lwr), color = "red", linetype = "dashed")+
+  geom_line(aes(y = upr), color = "red", linetype = "dashed") +
+  labs(title = "Period Deaths, Confidence and Prediction Intervals for Puerto Rico, 2000-2017", subtitle = "Deaths for September - December", y = "Period Deaths", x ="Year",
+       caption = "Supplemental Figure for Response to Cruz-Cano and Mead in JAMA by Santos and Howard")+
+  scale_x_continuous(breaks = seq(2000, 2017, by = 2))
+
+# Show the figure
+p
+
+# In the following lines of code, I produce a Confidence Interval using the for the OLS model and the synthetic data.
+# The upper limit (upr) of observation seventheen interval produced using predic and the OLS model is substracted 
+# from the 2017 observation to calculate the number of deaths in excess. 
+
+conf.int2<-predict(model2,newdata=new_year,interval="confidence") #Calculate Confidence Intervals from OLS parameters
+
+mydata2<-cbind(deaths_raw,round(conf.int2,0)) #Create new data and round up to the closest integer
+
+#Subset the data for 2017 Only
+excess_data2<-subset(mydata2, Year>2016)
+excess2<-excess_data2$Period_Deaths-excess_data2$upr
+
+#Show excess calculation
+excess2
+
